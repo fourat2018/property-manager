96b7030e47d0a7adad81c92e18ea441562ecc0df
fourat1991 <euphrate1991@gmail.com>
2018-05-06 22:50:37 +0200

    JSON grammar of the response to the properties query GET/POST

---
+++ app/v1/properties/resource/PropertyResource.scala
@@ -0,0 +1,30 @@
+package  v1.properties.resource
+
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+
+/*
+* Json mapping of the resource sent in the response
+*
+ */
+
+case class PropertyResource(id: Long, address:String, postcode:String, latitude:Double,longitude:Double, bedroomCount:Option[Int],surface:Option[Double], link:String)
+
+object PropertyResource {
+
+
+  implicit val implicitWrites: Writes[PostResource] = (
+    (JsPath \ "id").write[Long] and
+      (JsPath \ "address").write[String] and
+      (JsPath \ "postcode").write[String] and
+      (JsPath \ "latitude").write[Double] and
+      (JsPath \ "longitude").write[Double] and
+      {JsPath \ "bedroom_count"}.writeNullable[Int] and
+      {JsPath \ "surface"}.writeNullable[Double] and
+      (JsPath \ "link").write[String]
+    )(unlift(PostResource.unapply))
+}
+
+

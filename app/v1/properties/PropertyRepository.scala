03075a943452a4ff5eeaca80e416135579569c4c
fourat1991 <euphrate1991@gmail.com>
2018-05-06 22:20:57 +0200

    Operations On the properties table in the database

--- app/v1/properties/PropertyRepository.scala
+++ app/v1/properties/PropertyRepository.scala
@@ -7,11 +7,12 @@ import play.api.libs.concurrent.CustomExecutionContext
 import play.api.{Logger, MarkerContext}
 import play.api.db.slick.DatabaseConfigProvider
 
-
 import scala.concurrent.Future
 
+import v1.properties.forms.PropertyFormInput
 
 
+final case class PropertyData(id: Long, address: String, postCode: String,latitude:Double,longitude:Double,bedroomCount:Option[Int],surface:Option[Double])
 
 
 class PropertyExecutionContext @Inject()(actorSystem: ActorSystem) extends CustomExecutionContext(actorSystem, "repository.dispatcher")
@@ -19,6 +20,17 @@ class PropertyExecutionContext @Inject()(actorSystem: ActorSystem) extends Custo
 
 
 trait PropertyRepository {
+
+  def createProperty(data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
+
+  def listProperties()(implicit mc: MarkerContext): Future[Seq[PropertyData]]
+
+  def getProperty(id: Long)(implicit mc: MarkerContext): Future[Option[PropertyData]]
+
+  def deleteProperty(id:Long) (implicit mc: MarkerContext):Future[Int]
+
+  def updateProperty(id:Long,data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
+
 
 }
 

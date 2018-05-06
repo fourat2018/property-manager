89120ce0a8689f3a03a19888bbad247625e9cc2a
fourat1991 <euphrate1991@gmail.com>
2018-05-06 22:35:08 +0200

    {slick} table properties definition

--- app/v1/properties/PropertyRepository.scala
+++ app/v1/properties/PropertyRepository.scala
@@ -6,13 +6,17 @@ import akka.actor.ActorSystem
 import play.api.libs.concurrent.CustomExecutionContext
 import play.api.{Logger, MarkerContext}
 import play.api.db.slick.DatabaseConfigProvider
+import java.sql.Date
 
-import scala.concurrent.Future
+import slick.jdbc.JdbcProfile
+
 
 import v1.properties.forms.PropertyFormInput
 
 
+
 final case class PropertyData(id: Long, address: String, postCode: String,latitude:Double,longitude:Double,bedroomCount:Option[Int],surface:Option[Double])
+
 
 
 class PropertyExecutionContext @Inject()(actorSystem: ActorSystem) extends CustomExecutionContext(actorSystem, "repository.dispatcher")
@@ -21,15 +25,15 @@ class PropertyExecutionContext @Inject()(actorSystem: ActorSystem) extends Custo
 
 trait PropertyRepository {
 
-  def createProperty(data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
-
-  def listProperties()(implicit mc: MarkerContext): Future[Seq[PropertyData]]
-
-  def getProperty(id: Long)(implicit mc: MarkerContext): Future[Option[PropertyData]]
-
-  def deleteProperty(id:Long) (implicit mc: MarkerContext):Future[Int]
-
-  def updateProperty(id:Long,data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
+//  def createProperty(data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
+//
+//  def listProperties()(implicit mc: MarkerContext): Future[Seq[PropertyData]]
+//
+//  def getProperty(id: Long)(implicit mc: MarkerContext): Future[Option[PropertyData]]
+//
+//  def deleteProperty(id:Long) (implicit mc: MarkerContext):Future[Int]
+//
+//  def updateProperty(id:Long,data: PropertyFormInput)(implicit mc: MarkerContext): Future[PropertyData]
 
 
 }
@@ -37,5 +41,38 @@ trait PropertyRepository {
 
 @Singleton
 class PropertyRepositoryImpl @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: PropertyExecutionContext) extends PropertyRepository {
+
+  private val logger = Logger(this.getClass)
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+
+
+  private class PropertyTable(tag: Tag) extends Table[PropertyData](tag, "properties") {
+
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def address = column[String]("address")
+
+    def postcode = column[String]("postcode")
+
+    def latitude = column[Double]("latitude")
+
+    def longitude = column[Double]("longitude")
+
+    def bedroomCount = column[Option[Int]]("bedroom_count")
+
+    def surface = column[Option[Double]]{"surface"}
+
+
+
+    def * = (id, address, postcode,latitude,longitude,bedroomCount,surface) <> ((PropertyData.apply _).tupled, PropertyData.unapply)
+  }
+
+
+
+
 
 }

cdc3b9a9d064c25530a36f2ff3e1f88ac46829e1
AMAIISDOM\fferchichi <fourat.ferchichi@amadeus.com>
2018-05-07 13:50:08 +0200

    Brush tests on the delete property query

--- test/v1/properties/PropertyControllerSpec.scala
+++ test/v1/properties/PropertyControllerSpec.scala
@@ -21,12 +21,28 @@ class PropertyControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
 
     "accept a CREATE query with all the required fields" in {
-      val request = FakeRequest(POST, "/v1/properties").withHeaders(HOST -> "localhost:9000")
+      val request1 = FakeRequest(POST, "/v1/properties").withHeaders(HOST -> "localhost:9000")
         .withFormUrlEncodedBody(("address","toto"),("postcode","titi"),("latitude","12345.25"),("longitude","0"))
-      val home = route(app, request).get
+      val home1 = route(app, request1).get
 
-      contentAsString(home) must include ("/v1/properties/1\"")
+      contentAsString(home1) must include ("/v1/properties/1\"")
+
+
+      val request2 = FakeRequest(POST, "/v1/properties").withHeaders(HOST -> "localhost:9000")
+        .withFormUrlEncodedBody(("address","toto"),("postcode","titi"),("latitude","12345.25"),("longitude","0"))
+      val home2 = route(app, request2).get
+
+      contentAsString(home2) must include ("/v1/properties/2\"")
+
+
+      val request3 = FakeRequest(POST, "/v1/properties").withHeaders(HOST -> "localhost:9000")
+        .withFormUrlEncodedBody(("address","toto"),("postcode","titi"),("latitude","12345.25"),("longitude","0"))
+      val home3 = route(app, request3).get
+
+      contentAsString(home3) must include ("/v1/properties/3\"")
+
     }
+
 
     "return a created property" in {
       val request = FakeRequest(GET, "/v1/properties/1").withHeaders(HOST -> "localhost:9000")
@@ -41,6 +57,37 @@ class PropertyControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
       contentAsString(home) must include ("null")
     }
+
+
+    "accept a delete on a property that exists " in {
+      val request = FakeRequest(DELETE, "/v1/properties/1").withHeaders(HOST -> "localhost:9000")
+      val home = route(app, request).get
+
+      contentAsString(home) must include ("1")
+
+    }
+
+    "Ensure this property does not exists anymore" in {
+      val request = FakeRequest(GET, "/v1/properties/1").withHeaders(HOST -> "localhost:9000")
+      val home = route(app, request).get
+
+      contentAsString(home) must include ("null")
+    }
+
+
+    "reject a delete on a property that does not exist " in {
+      val request = FakeRequest(DELETE, "/v1/properties/1").withHeaders(HOST -> "localhost:9000")
+      val home = route(app, request).get
+
+      contentAsString(home) must include ("0")
+
+    }
+
+
+
+
+
+
 
 
 
